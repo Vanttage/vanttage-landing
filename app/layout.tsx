14a181e90496c34de58dc697f7a150888f664c3e
Moisés Ruiz <moisesruizruizsoraca@gmail.com>
2026-05-02 10:59:00 -0500

    se agrego la indexacion a google ads.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Syne, DM_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 import Navbar from "@/app/components/layout/Navbar";
@@ -134,8 +135,31 @@ export default function RootLayout({
         <meta name="geo.region" content="CO-BOL" />
         <meta name="geo.placename" content="Cartagena, Colombia" />
         <JsonLd />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KVHZXM6G');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className="bg-[#061729] text-[#F8FAFC] antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KVHZXM6G"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Navbar />
         {children}
         <Footer />
